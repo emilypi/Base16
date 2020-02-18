f0ace7411cc25d8e675ad002a0ca4f42d521ea9b
Emily Pillmore <emily@kadena.io>
2020-02-18 12:53:08 -0500

    updated docs

--- src/Data/ByteString/Base16.hs
+++ src/Data/ByteString/Base16.hs
@@ -57,6 +57,20 @@ decodeBase16 = decodeBase16_
 
 -- | Tell whether a 'ByteString' value is base16 encoded.
 --
+-- Examples:
+--
+-- This example will fail. It conforms to the alphabet, but
+-- is not valid because it has an incorrect (odd) length.
+--
+-- >>> isBase16 "666f6"
+-- False
+--
+-- This example will succeed because it satisfies the alphabet
+-- and is considered "valid" (i.e. of the correct size and shape).
+--
+-- >>> isBase16 "666f"
+-- True
+--
 isBase16 :: ByteString -> Bool
 isBase16 bs = isValidBase16 bs && isRight (decodeBase16 bs)
 {-# INLINE isBase16 #-}
@@ -64,8 +78,23 @@ isBase16 bs = isValidBase16 bs && isRight (decodeBase16 bs)
 -- | Tell whether a 'ByteString' value is a valid Base16 format.
 --
 -- This will not tell you whether or not this is a correct Base16 representation,
--- only that it conforms to the correct shape. To check whether it is a true
+-- only that it conforms to the correct alphabet. To check whether it is a true
 -- Base16 encoded 'ByteString' value, use 'isBase16'.
+--
+-- Examples:
+--
+-- This example will fail because it does not conform to the Hex
+-- alphabet.
+--
+-- >>> isValidBase16 "666f+/6"
+-- False
+--
+-- This example will succeed because it satisfies the alphabet
+-- and is considered "valid" (i.e. of the correct size and shape), but
+-- is not correct base16 because it is the wrong shape.
+--
+-- >>> isValidBase16 "666f6"
+-- True
 --
 isValidBase16 :: ByteString -> Bool
 isValidBase16 = validateBase16 "0123456789abcdef"
--- src/Data/Text/Encoding/Base16.hs
+++ src/Data/Text/Encoding/Base16.hs
@@ -42,6 +42,20 @@ decodeBase16 = fmap T.decodeUtf8 . B16.decodeBase16 . T.encodeUtf8
 
 -- | Tell whether a 'Text' value is Base16-encoded.
 --
+-- Examples:
+--
+-- This example will fail. It conforms to the alphabet, but
+-- is not valid because it has an incorrect (odd) length.
+--
+-- >>> isBase16 "666f6"
+-- False
+--
+-- This example will succeed because it satisfies the alphabet
+-- and is considered "valid" (i.e. of the correct size and shape).
+--
+-- >>> isBase16 "666f"
+-- True
+--
 isBase16 :: Text -> Bool
 isBase16 = B16.isBase16 . T.encodeUtf8
 {-# INLINE isBase16 #-}
@@ -51,6 +65,21 @@ isBase16 = B16.isBase16 . T.encodeUtf8
 -- This will not tell you whether or not this is a correct Base16 representation,
 -- only that it conforms to the correct shape. To check whether it is a true
 -- Base16 encoded 'Text' value, use 'isBase16'.
+--
+-- Examples:
+--
+-- This example will fail because it does not conform to the Hex
+-- alphabet.
+--
+-- >>> isValidBase16 "666f+/6"
+-- False
+--
+-- This example will succeed because it satisfies the alphabet
+-- and is considered "valid" (i.e. of the correct size and shape), but
+-- is not correct base16 because it is the wrong shape.
+--
+-- >>> isValidBase16 "666f6"
+-- True
 --
 isValidBase16 :: Text -> Bool
 isValidBase16 = B16.isValidBase16 . T.encodeUtf8
