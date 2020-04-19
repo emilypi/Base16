a88d2636ad373e3fc7bfd401234dddedfc9157b1
Emily Pillmore <emilypi@cohomolo.gy>
2020-04-18 22:14:59 -0400

    trim unused utils

--- src/Data/ByteString/Base16/Internal/Utils.hs
+++ src/Data/ByteString/Base16/Internal/Utils.hs
@@ -2,24 +2,14 @@
 {-# LANGUAGE MagicHash #-}
 module Data.ByteString.Base16.Internal.Utils
 ( aix
-, w32
-, w64
 , reChunk
-, writeNPlainForeignPtrBytes
 ) where
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 
-import Foreign.ForeignPtr
-import Foreign.Ptr
-import Foreign.Storable
-
 import GHC.Exts
-import GHC.ForeignPtr
 import GHC.Word
-
-import System.IO.Unsafe
 
 
 -- | Read 'Word8' index off alphabet addr
@@ -27,31 +17,6 @@ import System.IO.Unsafe
 aix :: Word8 -> Addr# -> Word8
 aix (W8# i) alpha = W8# (indexWord8OffAddr# alpha (word2Int# i))
 {-# INLINE aix #-}
-
-w32 :: Word8 -> Word32
-w32 = fromIntegral
-{-# INLINE w32 #-}
-
-w64 :: Word8 -> Word64
-w64 = fromIntegral
-{-# INLINE w64 #-}
-
--- | Allocate and fill @n@ bytes with some data
---
-writeNPlainForeignPtrBytes
-    :: ( Storable a
-       , Storable b
-       )
-    => Int
-    -> [a]
-    -> ForeignPtr b
-writeNPlainForeignPtrBytes !n as = unsafeDupablePerformIO $ do
-    fp <- mallocPlainForeignPtrBytes n
-    withForeignPtr fp $ \p -> go p as
-    return (castForeignPtr fp)
-  where
-    go !_ [] = return ()
-    go !p (x:xs) = poke p x >> go (plusPtr p 1) xs
 
 -- | Form a list of chunks, and rechunk the list of bytestrings
 -- into length multiples of 2
