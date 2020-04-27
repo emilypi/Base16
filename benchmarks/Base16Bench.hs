36321e0fa64ea9bb9601a03bae4164b8871ef8cc
Emily Pillmore <emilypi@cohomolo.gy>
2020-04-27 01:15:56 -0400

    bench short-bytestring

--- benchmarks/Base16Bench.hs
+++ benchmarks/Base16Bench.hs
@@ -7,10 +7,10 @@ module Main
 import Criterion
 import Criterion.Main
 
-import Data.ByteString.Lazy (fromStrict)
-import "memory" Data.ByteArray.Encoding as Mem
 import Data.ByteString
+import Data.ByteString.Short
 import "base16" Data.ByteString.Base16 as B16
+import "base16" Data.ByteString.Short.Base16 as BS16
 import "base16-bytestring" Data.ByteString.Base16 as Bos
 import Data.ByteString.Random (random)
 
@@ -21,32 +21,32 @@ main =
     [ env bs $ \ ~((bs25,bs100,bs1k,bs10k,bs100k,bs1mm),(bs25L,bs100L,bs1kL,bs10kL,bs100kL,bs1mmL)) ->
       bgroup "encode"
       [ bgroup "25"
-        [ bench "memory" $ whnf ctob bs25
+        [ bench "base16-short" $ whnf BS16.encodeBase16' bs25L
         , bench "base16-bytestring" $ whnf Bos.encode bs25
         , bench "base16" $ whnf B16.encodeBase16' bs25
         ]
       , bgroup "100"
-        [ bench "memory" $ whnf ctob bs100
+        [ bench "base16-short" $ whnf BS16.encodeBase16' bs100L
         , bench "base16-bytestring" $ whnf Bos.encode bs100
         , bench "base16" $ whnf B16.encodeBase16' bs100
         ]
       , bgroup "1k"
-        [ bench "memory" $ whnf ctob bs1k
+        [ bench "base16-short" $ whnf BS16.encodeBase16' bs1kL
         , bench "base16-bytestring" $ whnf Bos.encode bs1k
         , bench "base16" $ whnf B16.encodeBase16' bs1k
         ]
       , bgroup "10k"
-        [ bench "memory" $ whnf ctob bs10k
+        [ bench "base16-short" $ whnf BS16.encodeBase16' bs10kL
         , bench "base16-bytestring" $ whnf Bos.encode bs10k
         , bench "base16" $ whnf B16.encodeBase16' bs10k
         ]
       , bgroup "100k"
-        [ bench "memory" $ whnf ctob bs100k
+        [ bench "base16-short" $ whnf BS16.encodeBase16' bs100kL
         , bench "base16-bytestring" $ whnf Bos.encode bs100k
         , bench "base16" $ whnf B16.encodeBase16' bs100k
         ]
       , bgroup "1mm"
-        [ bench "memory" $ whnf ctob bs1mm
+        [ bench "base16-short" $ whnf BS16.encodeBase16' bs1mmL
         , bench "base16-bytestring" $ whnf Bos.encode bs1mm
         , bench "base16" $ whnf B16.encodeBase16' bs1mm
         ]
@@ -54,44 +54,38 @@ main =
     , env bs' $ \ ~((bs25,bs100,bs1k,bs10k,bs100k,bs1mm),(bs25L,bs100L,bs1kL,bs10kL,bs100kL,bs1mmL)) ->
       bgroup "decode"
       [ bgroup "25"
-        [ bench "memory" $ whnf cfob bs25
+        [ bench "base16-short" $ whnf BS16.decodeBase16 bs25L
         , bench "base16-bytestring" $ whnf Bos.decode bs25
-        , bench "base16" $ whnf B16.decodeBase16Lenient bs25
+        , bench "base16" $ whnf B16.decodeBase16 bs25
         ]
       , bgroup "100"
-        [ bench "memory" $ whnf cfob bs100
+        [ bench "base16-short" $ whnf BS16.decodeBase16 bs100L
         , bench "base16-bytestring" $ whnf Bos.decode bs100
-        , bench "base16" $ whnf B16.decodeBase16Lenient bs100
+        , bench "base16" $ whnf B16.decodeBase16 bs100
         ]
       , bgroup "1k"
-        [ bench "memory" $ whnf cfob bs1k
+        [ bench "base16-short" $ whnf BS16.decodeBase16 bs1kL
         , bench "base16-bytestring" $ whnf Bos.decode bs1k
-        , bench "base16" $ whnf B16.decodeBase16Lenient bs1k
+        , bench "base16" $ whnf B16.decodeBase16 bs1k
         ]
       , bgroup "10k"
-        [ bench "memory" $ whnf cfob bs10k
+        [ bench "base16-short" $ whnf BS16.decodeBase16 bs10kL
         , bench "base16-bytestring" $ whnf Bos.decode bs10k
-        , bench "base16" $ whnf B16.decodeBase16Lenient bs10k
+        , bench "base16" $ whnf B16.decodeBase16 bs10k
         ]
       , bgroup "100k"
-        [ bench "memory" $ whnf cfob bs100k
+        [ bench "base16-short" $ whnf BS16.decodeBase16 bs100kL
         , bench "base16-bytestring" $ whnf Bos.decode bs100k
-        , bench "base16" $ whnf B16.decodeBase16Lenient bs100k
+        , bench "base16" $ whnf B16.decodeBase16 bs100k
         ]
       , bgroup "1mm"
-        [ bench "memory" $ whnf cfob bs1mm
+        [ bench "base16-short" $ whnf BS16.decodeBase16 bs1mmL
         , bench "base16-bytestring" $ whnf Bos.decode bs1mm
-        , bench "base16" $ whnf B16.decodeBase16Lenient bs1mm
+        , bench "base16" $ whnf B16.decodeBase16 bs1mm
         ]
       ]
     ]
   where
-    ctob :: ByteString -> ByteString
-    ctob = Mem.convertToBase Mem.Base16
-
-    cfob :: ByteString -> Either String ByteString
-    cfob = Mem.convertFromBase Mem.Base16
-
     bs = do
       a <- random 25
       b <- random 100
@@ -99,13 +93,13 @@ main =
       d <- random 10000
       e <- random 100000
       f <- random 1000000
-      return ((a,b,c,d,e,f),(fromStrict a,fromStrict b,fromStrict c,fromStrict d,fromStrict e,fromStrict f))
+      return ((a,b,c,d,e,f),(toShort a,toShort b,toShort c,toShort d,toShort e,toShort f))
 
     bs' = do
-      a <- ctob <$> random 25
-      b <- ctob <$> random 100
-      c <- ctob <$> random 1000
-      d <- ctob <$> random 10000
-      e <- ctob <$> random 100000
-      f <- ctob <$> random 1000000
-      return ((a,b,c,d,e,f),(fromStrict a,fromStrict b,fromStrict c,fromStrict d,fromStrict e,fromStrict f))
+      a <- B16.encodeBase16' <$> random 25
+      b <- B16.encodeBase16' <$> random 100
+      c <- B16.encodeBase16' <$> random 1000
+      d <- B16.encodeBase16' <$> random 10000
+      e <- B16.encodeBase16' <$> random 100000
+      f <- B16.encodeBase16' <$> random 1000000
+      return ((a,b,c,d,e,f),(toShort a,toShort b,toShort c,toShort d,toShort e,toShort f))
