2465977f0a5185a52432241a9ea8c1a9d5cf1024
Emily Pillmore <emilypi@cohomolo.gy>
2020-07-23 11:33:38 -0400

    update nix

---
+++ default.nix
@@ -0,0 +1,41 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, base16-bytestring, bytestring
+      , criterion, deepseq, primitive, QuickCheck, random-bytestring
+      , stdenv, tasty, tasty-hunit, tasty-quickcheck, text, text-short
+      }:
+      mkDerivation {
+        pname = "base16";
+        version = "0.3.0.0";
+        src = ./.;
+        libraryHaskellDepends = [
+          base bytestring deepseq primitive text text-short
+        ];
+        testHaskellDepends = [
+          base base16-bytestring bytestring QuickCheck random-bytestring
+          tasty tasty-hunit tasty-quickcheck text text-short
+        ];
+        benchmarkHaskellDepends = [
+          base base16-bytestring bytestring criterion deepseq
+          random-bytestring text
+        ];
+        homepage = "https://github.com/emilypi/base16";
+        description = "Fast RFC 4648-compliant Base16 encoding";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
