ba99dc8607d1740930cd69e9e39aa35a62f2b89f
Emily Pillmore <emilypi@cohomolo.gy>
2022-12-27 20:17:25 -0700

    Add type for Base16

---
+++ src/Data/Base16/Types.hs
@@ -0,0 +1,42 @@
+{-# language ExplicitNamespaces #-}
+{-# language Safe #-}
+-- |
+-- Module       : Data.ByteString.Base16.Types
+-- Copyright    : (c) 2019-2022 Emily Pillmore
+-- License      : BSD-style
+--
+-- Maintainer   : Emily Pillmore <emilypi@cohomolo.gy>,
+--                sofia-m-a <https://github.com/sofia-m-a>
+-- Stability    : stable
+-- Portability  : non-portable
+--
+-- This module contains the 'Base16' type definition, 'Alphabet'
+-- datatype, alphabet constraints, and various quality of life
+-- combinators for working with 'Base16'-wrapped data.
+--
+module Data.Base16.Types
+( type Base16
+, assertBase16
+, extractBase16
+) where
+
+
+import Data.Base16.Types.Internal (Base16(..))
+
+-- | Assert a value to be encoded in a specific way
+--
+-- /Warning/: This is a blind assertion that a particular
+-- value is base16 encoded in some alphabet. If you are not
+-- sure of the provenance of the value, you may experience
+-- odd behavior when attempting to decode. Use at your own
+-- risk. If I see any issues logged on this project from
+-- negligent use of this or 'coerceBase16', Sofia and I will
+-- smite you.
+--
+assertBase16 :: a -> Base16 a
+assertBase16 = Base16
+
+-- | Forget that a particular value is Base16-encoded
+--
+extractBase16 :: Base16 a -> a
+extractBase16 (Base16 a) = a
---
+++ src/Data/Base16/Types/Internal.hs
@@ -0,0 +1,44 @@
+{-# language Safe #-}
+-- |
+-- Module       : Data.ByteString.Base16.Types.Internal
+-- Copyright    : (c) 2019-2022 Emily Pillmore
+-- License      : BSD-style
+--
+-- Maintainer   : Emily Pillmore <emilypi@cohomolo.gy>,
+--                sofia-m-a <https://github.com/sofia-m-a>
+-- Stability    : stable
+-- Portability  : non-portable
+--
+-- This module contains the 'Base16' newtype.
+--
+module Data.Base16.Types.Internal
+( Base16(..)
+) where
+
+
+import Control.DeepSeq
+
+
+-- | Wraps a value, asserting that it is or is intended to be
+-- in a particular kind of Base16 encoding use 'extractBase16'
+-- to extract the value, and 'assertBase16' to tag a value
+-- as base16-encoded
+--
+newtype Base16 a = Base16 a
+
+instance Functor Base16 where
+  fmap f (Base16 a) = Base16 (f a)
+
+instance Applicative Base16 where
+  pure = Base16
+  Base16 f <*> Base16 a = Base16 (f a)
+
+instance Monad Base16 where
+  return = pure
+  Base16 a >>= k = k a
+
+instance Show a => Show (Base16 a) where
+  show (Base16 a) = show a
+
+instance NFData a => NFData (Base16 a) where
+  rnf (Base16 a) = rnf a
